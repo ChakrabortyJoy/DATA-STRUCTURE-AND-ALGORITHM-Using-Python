e80fd5f6c38ba67279a8f51c12ca657bc0f9089b
Joy Chakraborty <joyhit19@gmail.com>
2022-01-31 12:22:06 +0530

    Add files via upload

--- QueueLinkedList.py
+++ QueueLinkedList.py
@@ -11,11 +11,7 @@ class LinkedList:
         self.head = None
         self.tail = None
     
-    def __iter__(self):
-        curNode = self.head
-        while curNode:
-            yield curNode
-            curNode = curNode.next
+    
 
 class Queue:
     def __init__(self):
@@ -65,10 +61,10 @@ class Queue:
 
 
 
-custQueue = Queue()
-custQueue.enqueue(2)
-custQueue.enqueue(4)
-custQueue.enqueue(6)
-print(custQueue)
-print(custQueue.peek())
-print(custQueue)
+#custQueue = Queue()
+#custQueue.enqueue(1)
+#custQueue.enqueue(2)
+#custQueue.enqueue(3)
+#print(custQueue)
+#print(custQueue.peek())
+#print(custQueue)
