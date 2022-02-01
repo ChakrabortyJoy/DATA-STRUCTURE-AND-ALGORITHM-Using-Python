925cbdf688bf881d2c275dbd2540b62a53eabf56
Joy Chakraborty <joyhit19@gmail.com>
2022-02-01 11:28:24 +0530

    Add files via upload

--- QueueLinkedList.py
+++ QueueLinkedList.py
@@ -61,10 +61,10 @@ class Queue:
 
 
 
-#custQueue = Queue()
-#custQueue.enqueue(1)
-#custQueue.enqueue(2)
-#custQueue.enqueue(3)
-#print(custQueue)
-#print(custQueue.peek())
-#print(custQueue)
+# custQueue = Queue()
+# custQueue.enqueue(1)
+# custQueue.enqueue(2)
+# custQueue.enqueue(3)
+# print(custQueue)
+# print(custQueue.peek())
+# print(custQueue)
